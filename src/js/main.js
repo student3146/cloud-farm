d36e4dc1e5e6f83da27b3b592f7a4715f2abe70c
Igor Kostiuk <iskostiuk3146@gmail.com>
2025-12-23 04:03:22 +0200

    responsive

--- src/js/main.js
+++ src/js/main.js
@@ -9,12 +9,32 @@ const app = {
   modules() {
     document.addEventListener('DOMContentLoaded', () => {
       var swiperBanner = new Swiper(".swiper-banner", {
-        direction: 'horizontal',
+        direction: "horizontal",
         loop: true,
+        speed: 600,
         pagination: {
-          el: ".swiper-banner-pagination",
+          el: '.swiper-banner-pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '"></span>';
+          },
         },
+        on: {
+          resize: function () {
+            this.updateAutoHeight(300);
+          }
+        }
       });
+
+
+      const btnMobile = document.querySelector('.btn-mobile');
+      const mobileMenu = document.querySelector('.header-mobile-menu')
+      if (btnMobile && mobileMenu) {
+        btnMobile.addEventListener('click', () => {
+          btnMobile.classList.toggle('open')
+          mobileMenu.classList.toggle('open')
+        })
+      }
     });
   },
 }.init();
